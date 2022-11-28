bd859587fbfd385d8990774a4fa9bc82a300cb82
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-11-28 17:31:20 +0330

    feat: create a user with post

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+
+            'email' => 'amirfz878787@gmail.com',
+            'password' => Hash::make('amir8787')
+        ]);
+
+        $user->posts()->create([
+            'title' => 'post1',
+            'description' => 'post desc'
+        ]);
+    }
+}
