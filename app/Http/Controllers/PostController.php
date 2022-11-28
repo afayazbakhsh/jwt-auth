0a57fc5717f9da27af910262105fd62a14ccbca6
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-11-28 17:34:29 +0330

    feat: a siple method for check auth

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = $this->authUser();
+
+        return $user->posts;
+    }
+
+}
