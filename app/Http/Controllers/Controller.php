78e7b655d45386813f18ea1c51278cd8277f54f3
amirfayazbakhsh <a.fayazbakhsh1@gmail.com>
2022-11-28 17:33:39 +0330

    feat: add a function for check user auth

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,16 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function authUser()
+    {
+        try {
+
+            return auth()->userOrFail();
+
+        } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+
+            return response()->json(['error' => $e->getMessage()]);
+        }
+    }
 }
